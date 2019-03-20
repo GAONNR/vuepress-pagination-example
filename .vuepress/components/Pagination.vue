d45e19ac37e79743c1fdfcb1d3380c03a8737f4b
Nick Neisen <nwneisen@gmail.com>
2019-03-20 14:14:55 -0600

    Initial commit

---
+++ .vuepress/components/Pagination.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div v-for="post in this.$site.pages" :key="post.id" :to="post.path"> 
+            {{ post.title }}
+        </div>
+        
+       <Content/>
+    </div>
+</template>
---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+    },
+    plugins: [ 
+      '@vuepress/pagination', { 
+        postsFilter: (({ type }) => type === 'post') 
+      }
+    ] 
+ }
